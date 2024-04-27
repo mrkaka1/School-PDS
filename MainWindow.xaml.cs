5bbb0be7bdd009d30bd6bc3dc4c9128a48deff54
Kauan <daluzmarkes@gmail.com>
2024-04-27 16:13:50 -0400

    commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sistem_School
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dashboards dashboard = new dashboards();
+            dashboard.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();   
+        }
+    }
+}
---
+++ Pages/dashboards.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sistem_School
+{
+    /// <summary>
+    /// Lógica interna para dashboards.xaml
+    /// </summary>
+    public partial class dashboards : Window
+    {
+        public dashboards()
+        {
+            InitializeComponent();
+        }
+
+        private void txbTeste_Click(object sender, RoutedEventArgs e)
+        {
+            framePage.Source = new Uri("./School.xaml", UriKind.Relative);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            framePage.Source = new Uri("./courses.xaml", UriKind.Relative);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            framePage.Source = new Uri("./class.xaml", UriKind.Relative);
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            framePage.Source = new Uri("./students.xaml", UriKind.Relative);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            framePage.Source = new Uri("./Reports.xaml", UriKind.Relative);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            framePage.Source = new Uri("./tables.xaml", UriKind.Relative);
+        }
+    }
+}
